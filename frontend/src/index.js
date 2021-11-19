0a45feaab716ecb30e2911abf1ba1b9849adf6eb
elaisa91 <elisatriolo91@gmail.com>
2021-11-19 12:50:23 +0100

    redux initialized

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,13 +1,34 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
 import './fonts/Junicode.ttf';
 import './index.css';
 import SearchPage from './components/SearchPage/SearchPage.js';
 
+const initialState = {
+   
+}
+
+function reducer (state = initialState, action) { 
+  switch(action.type) {
+    case "":
+      return {
+        ...state,
+        users: action.payload,
+      };
+    default:
+      return state;
+  }
+}
+
+export const store = createStore(reducer);
 
 ReactDOM.render(
   <React.StrictMode>
-    <SearchPage />
+    <Provider store={store}>
+      <SearchPage />
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
