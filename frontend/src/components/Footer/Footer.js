718cae8be1da942799d4873ab27b929ded546907
elaisa91 <elisatriolo91@gmail.com>
2022-01-07 16:00:31 +0100

    added informations footer

--- frontend/src/components/Footer/Footer.js
+++ frontend/src/components/Footer/Footer.js
@@ -7,6 +7,14 @@ class Footer extends React.Component{
             <div className='footer'>
                 <footer>
                     <img src={process.env.PUBLIC_URL + '/university_logo_footer.png'} alt="Università di Verona" />
+                    <div className='info'>
+                        <p>Università degli Studi di Verona</p>
+                        <p>Via dell'Artigliere, 8</p>
+                        <p>37129, Verona</p>
+                    </div>
+                    <div className='copyright'>
+                        <p>© 2022, Università di Verona. Tutti i diritti riservati.</p>
+                    </div>
                 </footer>
             </div>
         );
