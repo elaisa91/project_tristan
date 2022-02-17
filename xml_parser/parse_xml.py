a7c6f86aa41aab9cc941f9c5a34d4f689d9d46e7
elaisa91 <elisatriolo91@gmail.com>
2022-02-17 20:01:09 +0100

    xml parser uploaded

--- xml_parser/parse_xml.py
+++ xml_parser/parse_xml.py
@@ -7,9 +7,9 @@ import string
 
 myclient = pymongo.MongoClient("mongodb://localhost:27017/")
 mydb = myclient["facsimile_db"]
-mycol = mydb["facsimile_img_2"]
+mycol = mydb["facsimile_img_3"]
 
-tree = ET.parse('15v_completo_TEST.xml')
+tree = ET.parse('7r7v10r10v.xml')
 root = tree.getroot()
 
 def generate_char(ref, root):
@@ -32,25 +32,44 @@ def generate_hand(ref, root):
 
 def generate_transcription(trans_ide, root):
     transcription = {}
-    text = ""
+    transcription["text"] = {}
+    transcription["said"] = []
+    transcription["style"] = ""
+    transcription["type"] = ""
+    transcription["lang"] = ""
     for ab in root[2].iter("{http://www.tei-c.org/ns/1.0}ab"):
-        if (ab.attrib["{http://www.w3.org/XML/1998/namespace}id"] == trans_ide):
-            if ab.text:
-                text+= ab.text.replace("|", "\n")
-            for child in ab:
-                if (child.tag == "{http://www.tei-c.org/ns/1.0}g"):
-                    ref = child.attrib["ref"][1:]
-                    text+= generate_char(ref, root)
-                if (child.text):
-                    text+= child.text.replace("|", "\n")
-                if (child.tail):
-                    text+= child.tail.replace("|", "\n")
-            transcription["text"] = text
-            if "style" in ab.attrib:
-                transcription["style"] = string.capwords(ab.attrib["style"].replace("_", " "))
-            if "type" in ab.attrib:
-                ref = ab.attrib["type"][1:]
-                transcription["hand"] = generate_hand(ref, root)
+        choice = ab.find("{http://www.tei-c.org/ns/1.0}choice")
+        if (ab.attrib["{http://www.w3.org/XML/1998/namespace}id"] == trans_ide and choice != None):
+            for option in choice:
+                text = "" 
+                if option.text:
+                    text+= option.text.replace("|", "\n")
+                for child in option:
+                    if (child.tag == "{http://www.tei-c.org/ns/1.0}g"):
+                        ref = child.attrib["ref"][1:]
+                        text+= generate_char(ref, root)
+                    if (child.text):
+                        text+= child.text.replace("|", "\n")
+                    if (child.tail):
+                        text+= child.tail.replace("|", "\n")
+                if (option.tag == "{http://www.tei-c.org/ns/1.0}orig"):
+                    transcription['Orig'] = text
+                if (option.tag == "{http://www.tei-c.org/ns/1.0}reg"):
+                    transcription['Reg'] = text
+        if "style" in ab.attrib:
+            transcription["style"] = string.capwords(ab.attrib["style"].replace("_", " "))
+        if "type" in ab.attrib:
+            transcription["type"] = string.capwords(ab.attrib["type"].replace("_", " "))
+        if "{http://www.w3.org/XML/1998/namespace}lang" in ab.attrib:
+            transcription["lang"] = string.capwords(ab.attrib["{http://www.w3.org/XML/1998/namespace}lang"].replace("_", " "))
+        #if ab.findall("{http://www.tei-c.org/ns/1.0}said"):
+        for said in ab.findall("{http://www.tei-c.org/ns/1.0}said"):
+            said_obj = {}
+            if "who" in said.attrib:
+                said_obj["who"] = said.attrib["who"][1:]
+            if "toWhom" in said.attrib:
+                said_obj["toWhom"] = said.attrib["toWhom"][1:]
+            transcription["said"].append(said_obj)
     return (transcription)
 
 def generate_category(cat_ide, root):
@@ -64,6 +83,8 @@ def generate_category(cat_ide, root):
 
 def generate_subcategory(subcat_ide, root):
     subcategory = {}
+    subcategory["desc"] = ""
+    subcategory["name"] = ""
     if (subcat_ide != ""):
         subcategory["name"] = string.capwords(subcat_ide.replace("_", " "))
     for interp in root[0].iter("{http://www.tei-c.org/ns/1.0}interp"):
@@ -72,7 +93,7 @@ def generate_subcategory(subcat_ide, root):
                 subcategory["desc"] = interp.attrib["sameAs"]
             else:
                 subcategory["desc"] = interp.text
-    for person in root[0].iter("{http://www.tei-c.org/ns/1.0}person"):
+    """for person in root[0].iter("{http://www.tei-c.org/ns/1.0}person"):
         if (person.attrib["{http://www.w3.org/XML/1998/namespace}id"] == subcat_ide):
             subcategory["desc"] = {}
             if (person.find("{http://www.tei-c.org/ns/1.0}persName") != None):
@@ -80,7 +101,7 @@ def generate_subcategory(subcat_ide, root):
             if (person.find("{http://www.tei-c.org/ns/1.0}occupation") != None):
                 subcategory["desc"]["occupation"] = string.capwords(person.find("{http://www.tei-c.org/ns/1.0}occupation").text)
             if (person.find("{http://www.tei-c.org/ns/1.0}sex") != None):
-                subcategory["desc"]["sex"] = string.capwords(person.find("{http://www.tei-c.org/ns/1.0}sex").text)
+                subcategory["desc"]["sex"] = string.capwords(person.find("{http://www.tei-c.org/ns/1.0}sex").text)"""
     return (subcategory)
 
 def generate_polygon(string):
@@ -116,9 +137,9 @@ def generate_document(surface):
     for zone in surface.findall("{http://www.tei-c.org/ns/1.0}zone"):
         ide = ""
         points = ""
-        category = ""
-        subcategory = ""
-        transcription = ""
+        category = {}
+        subcategory = {}
+        transcription = {}
         cat_ide = ""
         subcat_ide = ""
         trans_ide = ""
@@ -135,7 +156,10 @@ def generate_document(surface):
         
         # crea categoria e sottocategoria 
         if ("type" in zone.attrib):
-            category = string.capwords(zone.attrib["type"].replace("_", " "))
+            if (zone.attrib["type"] == "scene"):
+                category = "Episode"
+            else:
+                category = string.capwords(zone.attrib["type"].replace("_", " "))
             if ("sameAs" in zone.attrib):
                 subcat_ide = zone.attrib["sameAs"][1:]
         elif ("ana" in zone.attrib) and ("corresp" in zone.attrib):
@@ -146,8 +170,8 @@ def generate_document(surface):
             # crea trascrizione
             if ("sameAs" in zone.attrib):
                 trans_ide = zone.attrib["sameAs"][1:]
-                transcription = generate_transcription (trans_ide, root)
-        
+                
+        transcription = generate_transcription (trans_ide, root)
         subcategory = generate_subcategory(subcat_ide, root) 
         
         if (not category in document):
