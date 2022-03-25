2a85f6ff2c70d86aef1c35bee6b8868b0f0feac7
elaisa91 <elisatriolo91@gmail.com>
2022-03-25 11:10:38 +0100

    changed logo on header and footer

--- frontend/src/components/Header/Header.js
+++ frontend/src/components/Header/Header.js
@@ -7,12 +7,12 @@ class Header extends React.Component{
             <div className='header'>
                 <div className='info'>
                     <p>A Scholarly Digital Edition</p>
-                    <p>of Gottfried von Straßburg's Tristan</p>
+                    <p>of Gottfried von Strassburg's Tristan</p>
                     <p>in München, Bayerischen Staatsbibliothek, Cgm 51</p>
                 </div>
-                <div className='img'>
+                {/*<div className='img'>
                     <img src={process.env.PUBLIC_URL + '/university_logo.png'} alt="Università di Verona" />
-                </div>
+                </div>*/}
             </div>
         );
     }
